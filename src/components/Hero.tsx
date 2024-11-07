3c8dce5295de898f17347cf3ebefb5a9bfa126e7
Sabashahid689 <nabegashahid@gmail.com>
2024-11-08 00:20:29 +0500

    update porfolio
    portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/logo.jpg)] bg-cover rounded-full'
-    style={{backgroundSize: "35%" , backgroundPosition: "left 100px top 100px"}}
+    style={{backgroundSize: "35%" , backgroundPosition: "left 100px top 100px",}}
     >
 
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)'>
